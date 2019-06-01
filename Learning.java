807c163df680be5b7be93711cdc88901b18fa5a3
“adithya2286” <bandamadithya@gmail.com>
2019-06-01 18:10:57 +0200

    modifictations for branch2

--- Learning.java
+++ Learning.java
@@ -1,2 +1,4 @@
-1)Created text file from local and not yet pushed
-2)Made changes to branch 2
+1)Created text file from local and not yet pushed.
+2)Changed master file at remote
+3)Change third line at Branch2
+4)Changed 4th line for Branch2
