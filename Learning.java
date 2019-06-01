a1d5edb637df2d59a2c6b06a71fcf7eb630d1283
Adithya Bandam <50749383+adithya2286@users.noreply.github.com>
2019-06-01 18:19:04 +0200

    Branch2 to master (#3)
    * change in branch2
    
    *  added line 2 from local system in Branch3
    
    * added third line at local Branch3
    
    *  modifictations for branch2

--- Learning.java
+++ Learning.java
@@ -1,4 +1,4 @@
 1)Created text file from local and not yet pushed.
-2)Modified master file at remote
-3)Modifiing third line at Branch4
-4)Extra 4th line for Branch4
+2)Changed master file at remote
+3)Change third line at Branch2
+4)Changed 4th line for Branch2
